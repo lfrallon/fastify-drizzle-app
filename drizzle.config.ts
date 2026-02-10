cea7be8739ab3c1f581a34df66e26d3aadeb6efe
lfrallon <loretofrallon@gmail.com>
2026-02-10 18:04:45 +0800

    chore: 🔧 updated tsconfig file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,7 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
+
+dotenv.config({ path: [".env.local", ".env"] });
 
 export default defineConfig({
   schema: "./src/drizzle/schema/index.ts",
