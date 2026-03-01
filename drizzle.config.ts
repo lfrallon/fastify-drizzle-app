caefe451e7eb2474c701e2fdbab5d819ed6ffb6e
lfrallon <loretofrallon@gmail.com>
2026-03-01 20:23:17 +0800

    chore(config): 🔧 updated db credentail connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,14 +8,7 @@ export default defineConfig({
   out: "./src/drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    database: process.env.POSTGRES_DB as string,
-    user: process.env.POSTGRES_USER as string,
-    password: process.env.POSTGRES_PASSWORD as string,
-    host: process.env.POSTGRES_HOST as string,
-    port: Number(process.env.POSTGRES_PORT),
-    // Alternatively, you can use a connection string:
-    //
-    // url: process.env.DATABASE_URL as string,
+    url: process.env.DATABASE_URL as string,
   },
   verbose: true,
   strict: true,
